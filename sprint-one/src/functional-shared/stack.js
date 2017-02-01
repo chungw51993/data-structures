5701d444ecb8de1f6a27053b3484574dca3e49fa
chungw51993 <chungw51993@gmail.com>
2017-02-01 13:31:11 -0600

    Implemented functional shared Stack

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstanceOf = {};
+  someInstanceOf.length = 0;
+  someInstanceOf.storage = {};
+  extend(someInstanceOf, stackMethods);
+
+  
+  return someInstanceOf;
+};
+
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.size = function() {
+  return this.length;
+};
 
+stackMethods.push = function(value) {
+  this.storage[this.length] = value;
+  this.length++;
+};
+
+stackMethods.pop = function(value) {
+  var answer = this.storage[this.length - 1];
+  if (this.length !== 0) {
+    this.length--;
+  }
+  return answer;
+};
