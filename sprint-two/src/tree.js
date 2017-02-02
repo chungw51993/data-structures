886a47b69032b7dc73d32418054645d19da5aa4a
chungw51993 <chungw51993@gmail.com>
2017-02-02 12:20:11 -0600

    Implemented Tree function

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,37 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  extend(newTree, treeMethods);
 
   return newTree;
+};
+
+var extend = function(to, from) {
+  for (var keys in from) {
+    to[keys] = from[keys];
+  }
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].value === target) {
+      return true;
+    } else if (this.children[i].children.length !== 0) {
+      for (var j = 0; j < this.children[i].children.length; j++) {
+        if (this.children[i].contains(target)) {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
 };
 
 
