a4bdda26cb7d591a6615e94aeabfa74e1e062f3b
chungw51993 <chungw51993@gmail.com>
2017-02-02 19:05:23 -0600

    Finished working on Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,99 +2,101 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  this.storage = [];
+  this.allNodes = [];
+  this.edgeList = [];
 };
 
 // Add a node to the graph, passing in the node's value.
-Graph.prototype.addNode = function(nodeObj) {
-  var node = {};
+Graph.prototype.addNode = function(node) {
+  var nodeObj = {};
+  nodeObj.value = node;
 
-  node.value = nodeObj;
-  node.connected = [];
- 
-
-  this.storage.push(node);
+  this.allNodes.push(nodeObj);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  for (var i = 0; i < this.storage.length; i++) {
-    if (this.storage[i].value === node) {
+  for (var i = 0; i < this.allNodes.length; i++) {
+    if (this.allNodes[i].value === node) {
       return true;
-    } 
+    }
   }
   return false;
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  for (var i = 0; i < this.storage.length; i++) {
-    if (this.storage[i].value === node) {
-      this.storage.splice(i, 1);
+  var index = -1;
+  for (var i = 0; i < this.allNodes.length; i++) {
+    if (this.allNodes[i].value === node) {
+      index = i;
     }
   }
+  this.allNodes.splice(index, 1);
+  for (var j = 0; j < this.edgeList.length; j++) {
+    if (this.edgeList[j][0] === node) {
+      this.edgeList.splice(j, 2);
+    }
+  }
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-
-  var contain = function(array, value) {
-    for (var i = 0; i < array.length; i++) {
-      if (array[i].value === value) {
-        return true;
-      } 
+  for (var i = 0; i < this.edgeList.length; i++) {
+    if (this.edgeList[i][0] === fromNode && this.edgeList[i][1] === toNode 
+      || this.edgeList[i][0] === toNode && this.edgeList[i][1] === fromNode ) {
+      return true;
     }
-    return false;
-  };
-
-  var startNode = this.indexOf(fromNode);
-  var lastNode = this.indexOf(toNode);
-
-  var mainArray = this.storage;
-  var firstArr = mainArray[startNode].connected;
-  var secondArr = mainArray[lastNode].connected;
-
-  if (contain(firstArr, toNode) && contain(secondArr, fromNode)) {
-    return true;
   }
   return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  var start = this.indexOf(fromNode);
-  var last = this.indexOf(toNode);
-
-  this.storage[start].connected.push(this.storage[last]);
-  this.storage[last].connected.push(this.storage[start]);
-
-  //console.log(this.storage[start].connected);
+  var fromIndex, toIndex;
+  for (var i = 0; i < this.allNodes.length; i++) {
+    if (this.allNodes[i].value === fromNode) {
+      fromIndex = i;
+    }
+  }
+  for (var j = 0; j < this.allNodes.length; j++) {
+    if (this.allNodes[j].value === toNode) {
+      toIndex = j;
+    }
+  }
+  var edge = [];
+  var flipEdge = [];
+  edge[0] = this.allNodes[fromIndex].value;
+  edge[1] = this.allNodes[toIndex].value;
+  flipEdge[0] = this.allNodes[toIndex].value;
+  flipEdge[1] = this.allNodes[fromIndex].value;
+  this.edgeList.push(edge);
+  this.edgeList.push(flipEdge);
+  console.log(this.edgeList);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  var mainArray = this.storage;
-  var indexFrom = mainArray.indexOf(fromNode);
-  var indexTo = mainArray.indexOf(toNode);
-  var fromChildrenArr = mainArray[indexFrom].connected;
-  var toChildrenArr = mainArray[indexTo].connected;
-  var indexToInFromArr = fromChildrenArr.indexOf(toNode);
-  var indexFromInToArr = toChildrenArr.indexOf(fromNode);
-
-  fromChildrenArr = fromChildrenArr.slice(indexToInFromArr, 1);
-  toChildrenArr = toChildrenArr.slice(indexFromInToArr, 1);
-
+  var edgeIndex, flipEdgeIndex;
+  for (var i = 0; i < this.edgeList.length; i++) {
+    if (this.edgeList[i][0] === fromNode && this.edgeList[i][1] === toNode) {
+      edgeIndex = i;
+    }
+  }
+  this.edgeList.splice(edgeIndex, 1);
+  for (var j = 0; j < this.edgeList.length; j++) {
+    if (this.edgeList[j][1] === fromNode && this.edgeList[j][0] === toNode) {
+      flipEdgeIndex = j;
+    }
+  }
+  this.edgeList.splice(flipEdgeIndex, 1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-};
-
-Graph.prototype.indexOf = function(value) {
-  for (var i = 0; i < this.storage.length; i++) {
-    if (this.storage[i].value === value) {
-      return i;
-    }
+  for (var i = 0; i < this.allNodes.length; i++) {
+    cb(this.allNodes[i].value);
   }
 };
 /*
