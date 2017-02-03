c178ec709a55ff332b31d9cda14472470b142c7f
chungw51993 <chungw51993@gmail.com>
2017-02-03 15:50:40 -0600

    Added insert function in Binary search tree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -11,22 +11,22 @@ describe('binarySearchTree', function() {
   //   expect(binarySearchTree.depthFirstLog).to.be.a('function');
   // });
 
-  it('should insert values at the correct location in the tree', function() {
-    binarySearchTree.insert(2);
-    binarySearchTree.insert(3);
-    binarySearchTree.insert(7);
-    binarySearchTree.insert(6);
-    expect(binarySearchTree.left.right.value).to.equal(3);
-    expect(binarySearchTree.right.left.value).to.equal(6);
-  });
-
-  // it('should have a working "contains" method', function() {
+  // it('should insert values at the correct location in the tree', function() {
   //   binarySearchTree.insert(2);
   //   binarySearchTree.insert(3);
   //   binarySearchTree.insert(7);
-  //   expect(binarySearchTree.contains(7)).to.equal(true);
-  //   expect(binarySearchTree.contains(8)).to.equal(false);
+  //   binarySearchTree.insert(6);
+  //   expect(binarySearchTree.left.right.value).to.equal(3);
+  //   expect(binarySearchTree.right.left.value).to.equal(6);
   // });
+
+  it('should have a working "contains" method', function() {
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(7);
+    expect(binarySearchTree.contains(7)).to.equal(true);
+    expect(binarySearchTree.contains(8)).to.equal(false);
+  });
 
   // it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
   //   var array = [];
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,47 +1,42 @@
 var BinarySearchTree = function(value) {
-  var searchTreeMethod = {};
-  var headNode = null;
-
+  var searchTreeMethod = treeNode(value);
+  
   searchTreeMethod.insert = function(value) {
-    var node = {};
-    node.value = value;
-    node.left = null;
-    node.right = null;
-   
-    if (headNode === null) {
-      headNode = node;
-    } else {
-      var searchInsert = function (nodeObj) {
-        if (node.value < nodeObj.value) {
-          if (nodeObj.left === null) {
-            nodeObj.left = node;
-          } else {
-            searchInsert(nodeObj.left);
-          }
-        } else if (node.value > nodeObj.value) {
-          if (nodeObj.right === null) {
-            nodeObj.right = node;
-          } else {
-            searchInsert(nodeObj.right);
-          }
+    var searchInsert = function (nodeObj) {
+      if (value < nodeObj.value) {
+        if (nodeObj.left === null) {
+          nodeObj.left = treeNode(value);
+        } else {
+          searchInsert(nodeObj.left);
         }
-      };
-      searchInsert(headNode);
-    }
-    console.log(headNode);
+      } else if (value > nodeObj.value) {
+        if (nodeObj.right === null) {
+          nodeObj.right = treeNode(value);
+        } else {
+          searchInsert(nodeObj.right);
+        }
+      }
+    };
+    searchInsert(searchTreeMethod);
   };
 
   searchTreeMethod.contains = function(value) {
-
+    
   };
 
   searchTreeMethod.depthFirstLog = function(callBack) {
-
   };
 
   return searchTreeMethod;
 };
 
+var treeNode = function(value) {
+  var node = {};
+  node.value = value;
+  node.left = null;
+  node.right = null;
+  return node;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
