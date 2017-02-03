e0825bec31eb4fabc5a437c89a67ca34d66f26aa
chungw51993 <chungw51993@gmail.com>
2017-02-03 12:13:15 -0600

    Working on binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,45 @@
 var BinarySearchTree = function(value) {
+  var searchTreeMethod = {};
+  var headNode = null;
+
+  searchTreeMethod.insert = function(value) {
+    var node = {};
+    node.value = value;
+    node.left = null;
+    node.right = null;
+   
+    if (headNode === null) {
+      headNode = node;
+    } else {
+      var searchInsert = function (nodeObj) {
+        if (node.value < nodeObj.value) {
+          if (nodeObj.left === null) {
+            nodeObj.left = node;
+          } else {
+            searchInsert(nodeObj.left);
+          }
+        } else if (node.value > nodeObj.value) {
+          if (nodeObj.right === null) {
+            nodeObj.right = node;
+          } else {
+            searchInsert(nodeObj.right);
+          }
+        }
+      };
+      searchInsert(headNode);
+    }
+    console.log(headNode);
+  };
+
+  searchTreeMethod.contains = function(value) {
+
+  };
+
+  searchTreeMethod.depthFirstLog = function(callBack) {
+
+  };
+
+  return searchTreeMethod;
 };
 
 
